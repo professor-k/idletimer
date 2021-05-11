f087c4417538072de58856c69148722cd327cb14
Andriy Kvasnytsya <andriy@kvasnytsya.name>
2021-05-11 15:48:22 +0300

    Initial commit

---
+++ IdleTimer/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace IdleTimer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(13, 13);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(38, 15);
+            this.label.TabIndex = 0;
+            this.label.Text = "label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(201, 60);
+            this.Controls.Add(this.label);
+            this.Name = "MainForm";
+            this.Text = "Idle Time";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ IdleTimer/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PInvoke.Win32;
+
+namespace IdleTimer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                label.Text = UserInput.IdleTime.ToString(@"hh\:mm\:ss\.fff");
+                await Task.Delay(10);
+            }
+        }
+    }
+}
